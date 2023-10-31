53cfc2b3124110b178a7a3a80e37d24fa797ddff
AmmosovPavel <ammosov.pavel.a@gmail.com>
2023-10-31 23:59:47 +0300

    Homework2.2

--- src/test/java/Army.java
+++ src/test/java/Army.java
@@ -1,11 +1,11 @@
 public class Army {
     public static void main(String[] args) {
-        int x = 1000;
-        int y = 1500;
+        final int LANNISTER_ARMY = 1000;
+        final int SARUMAN_ORKS = 1500;
 
-        int z = x + y;
-        System.out.println("У Ланнисторов в войске " + x + " пехотинцев");
-        System.out.println("У Сарумана в войске " + y + " орков");
-        System.out.println("Общая численность армии составляет " + z + " тел");
+        int TOTAL_ARMY = LANNISTER_ARMY + SARUMAN_ORKS;
+        System.out.println("У Ланнисторов в войске " + LANNISTER_ARMY + " пехотинцев");
+        System.out.println("У Сарумана в войске " + SARUMAN_ORKS + " орков");
+        System.out.println("Общая численность армии составляет " + TOTAL_ARMY + " тел");
     }
 }
