c8dc78d84ba88e9231808ca94e1e5c1d87e4bccd
AmmosovPavel <ammosov.pavel.a@gmail.com>
2023-10-31 23:49:01 +0300

    Homework2.2

---
+++ src/test/java/Army.java
@@ -0,0 +1,11 @@
+public class Army {
+    public static void main(String[] args) {
+        int x = 1000;
+        int y = 1500;
+
+        int z = x + y;
+        System.out.println("У Ланнисторов в войске " + x + " пехотинцев");
+        System.out.println("У Сарумана в войске " + y + " орков");
+        System.out.println("Общая численность армии составляет " + z + " тел");
+    }
+}
